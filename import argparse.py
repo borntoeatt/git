2f753e21b2fddfdd4dea5c7dea9c26af8693bba8
Dimitar Porkov <borntoeatt@gmail.com>
2025-09-05 13:00:27 +0300

    first commit

---
+++ import argparse.py
@@ -0,0 +1,21 @@
+import argparse
+from datetime import datetime
+
+class Greeter:
+    def __init__(self, name="World"):
+        self.name = name
+
+    def greet(self):
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        return f"[{current_time}] Hello, {self.name}!"
+
+def main():
+    parser = argparse.ArgumentParser(description="A fancy Hello World program.")
+    parser.add_argument("--name", type=str, help="Who to greet", default="World")
+    args = parser.parse_args()
+
+    greeter = Greeter(args.name)
+    print(greeter.greet())
+
+if __name__ == "__main__":
+    main()
